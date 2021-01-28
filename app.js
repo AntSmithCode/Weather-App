c56bda2cd4d220b3c3c5734cb371812c953dc13d
AntsmithCode <hey@antsmith.dev>
2021-01-27 22:31:30 -0500

    (App) MetaWeather Api Structure

---
+++ app.js
@@ -0,0 +1,38 @@
+const zipHttps = "https://api.zippopotam.us/country/state/city";
+const weatherHttps =
+  "https://www.metaweather.com/api/location/search/?lattlong=(latt),(long)";
+const zipWeatherElement = document.querySelector(".container");
+
+// Fetch zip code data
+async function getZipCode() {}
+
+// Use Fetched zip code data to Fetch zip code weather data
+
+/* Display Fetched zip code and weather data in Dom Elements
+
+<div class="row">
+    <!--For example, you would provide Zippopotamus with the data 90210 to get data for Beverly Hills. You can see the service
+            provides you with the name of the state and city-->
+    <div class="col">
+        <img src="imgs\weather-02-128.webp">
+    </div>
+    <!--Use the weather information to display the current temperature like this:-->
+    <div class="col">
+        <img src="imgs\weather-02-128.webp">
+    </div>
+</div>
+<!-- Use all the weather data you recieve from 7Timer to generate a 7 day forcast for the given zip code.-->
+<div class="row">
+    <div class="col">
+        <img src="imgs\weather-02-128.webp">
+    </div>
+    <div class="col">
+        <img src="imgs\weather-02-128.webp">
+    </div>
+    <div class="col">
+        <img src="imgs\weather-02-128.webp">
+    </div>
+</div>
+*/
+
+//
